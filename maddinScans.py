2d3a4b268129f54fa51042bfaa00af29422211ba
Daniel Schick <schick.daniel@gmail.com>
2018-06-29 09:13:13 +0200

    some user macros for scanning

--- maddinScans.py
+++ maddinScans.py
@@ -69,3 +69,41 @@ def fluenceDelayScan20180525(self):
     self.execMacro('ascan wp -5 55 60 1')
     self.execMacro('laserOff')
     self.execMacro('laserOff')
+    
+@macro()    
+def fluenceDelayScan20180606(self):
+    """Macro fluenceDelayScan20180606"""
+    self.output("Running fluenceDelayScan20180606...")
+
+    wpAngles = np.r_[0.004,0.012]
+    magCurrents = np.r_[10,20,30,40]
+    kepco = self.getMotor("kepco")
+    power = self.getPseudoMotor("power")
+    
+    
+    self.execMacro('laserOn')
+    self.execMacro('pumpOn')
+    self.execMacro('waittime 0')
+    
+    for wpAngle in wpAngles:
+        self.output(wpAngle)
+        
+        power.move(wpAngle)
+        
+        for magCurr in magCurrents:
+            self.output(magCurr)
+            kepco.move(-1*magCurr)
+            self.execMacro('regscan delay 1 -830 -825 5 -824.5 5 -823.8 35 -822 18 -820 10 -800 20 1000 18')
+            kepco.move(1*magCurr)
+            self.execMacro('regscan delay 1 -830 -825 5 -824.5 5 -823.8 35 -822 18 -820 10 -800 20 1000 18')
+        
+        
+    self.execMacro('pumpOff')
+    self.execMacro('laserOff')
+
+@macro() 
+def repeatHystereses20180625(self):
+    davidIsst = True
+    while(davidIsst):
+        self.execMacro("regscan kepco 1 -5 2 20 3 20 5 15 -1.5 20 -2.5 20 -5 10")
+
