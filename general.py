043405ec93cfa61222c295d0491d9311e33f4762
Daniel Schick <schick.daniel@gmail.com>
2018-07-04 17:08:27 +0200

    new fluenceconf macro
    also move setPowerParameters to general macros

--- general.py
+++ general.py
@@ -1,4 +1,4 @@
-from sardana.macroserver.macro import Macro, macro, Type
+from sardana.macroserver.macro import imacro, macro, Type
 import PyTango
 
 @macro([["time", Type.Float, None, "time in seconds"] ])
@@ -21,7 +21,66 @@ def magnsettings(self, ampl, waittime):
     self.output("magnampl set to %.2f A", ampl)    
     self.output("magnwaittime set to %.2f s", waittime)
 
+@imacro()
+def fluenceconf(self):
+    fluencePM = PyTango.DeviceProxy("pm/fluencectrl/1")
+    try:
+        lastHorFWHM = fluencePM.horFWHM
+        lastVerFWHM = fluencePM.verFWHM
+        lastRefl    = fluencePM.refl
+        lastRepRate = fluencePM.repRate
+    except:
+        lastHorFWHM = 100
+        lastVerFWHM = 100
+        lastRefl    = 0
+        lastRepRate = 3000
+        
     
+    
+    label, unit = "hor", "um"
+    horFWHM = self.input("What is the horizontal beam diameter (FWHM)?", data_type=Type.Float,
+                      title="Horizontal beam diameter", key=label, unit=unit,
+                      default_value=lastHorFWHM, minimum=0.0, maximum=100000)
+    label, unit = "ver", "um"
+    verFWHM = self.input("What is the vertical beam diameter (FWHM)?", data_type=Type.Float,
+                      title="Vertical beam diameter", key=label, unit=unit,
+                      default_value=lastVerFWHM, minimum=0.0, maximum=100000)
+    label, unit = "refl", "%"
+    refl = self.input("What is the sample reflectivity?", data_type=Type.Float,
+                      title="Sample reflectivity", key=label, unit=unit,
+                      default_value=lastRefl, minimum=0.0, maximum=100)
+    label, unit = "repRate", "Hz"
+    repRate = self.input("What is the laser repetition rate?", data_type=Type.Float,
+                      title="Laser repetition rate", key=label, unit=unit,
+                      default_value=lastRepRate, minimum=0.0, maximum=10000)
+    
+#    self.output("You selected a %s of %f %s", label, horFWHM, unit)
+#    self.output("You selected a %s of %f %s", label, horFWHM, unit)
+#    self.output("You selected a %s of %f %s", label, horFWHM, unit)
+#    self.output("You selected a %s of %f %s", label, horFWHM, unit)
+    
+    fluencePM.horFWHM = horFWHM
+    fluencePM.verFWHM = verFWHM
+    fluencePM.refl    = refl
+    fluencePM.repRate = repRate
+    
+    self.output("values saved!")
+
+@macro([["P0", Type.Float, None, "P0"],
+        ["Pm", Type.Float, None, "Pm"],
+        ["offset", Type.Float, None, "offset"],
+        ["period", Type.Float, None, "period"]])
+def setPowerParameter(self, P0, Pm, offset, period):
+    """This sets the parameters of the power pseudo motor"""
+    
+    power = PyTango.DeviceProxy("pm/powerctrl/1")
+    self.info('Update parameters of pseudo motor power')
+    power.offset = offset
+    power.period = period
+    power.P0     = P0
+    power.Pm     = Pm
+
+   
 @macro([["integ_time", Type.Float, None, "integration time"] ])
 def ct_altOn(self, integ_time):
     
